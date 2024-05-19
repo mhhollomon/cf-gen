344ca2322466f9d4ab2910bf42546b38e72590bf
Mark Hollomon <mhhollomon@gmail.com>
2024-05-18 20:03:55 -0400

    Initial commit
    Move old code to _cellar and put in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './cantus-icon.jpg';
 import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <header className="container-fluid gx-0 bg-primary-subtle">
+    <div className="row align-items-center m-3">
+      <img src={logo} height="100px" width="100px"
+        className="header-logo" />
+      <div className="col-8 text-center">
+        <span className="text-black fs-1 p-3 p-md-4">Cantus Fortuitus</span>
+      </div>
     </div>
+  </header>
+
+  <h3>Randomly Generated Cantus Firmus</h3>
+  <p>For all your counterpoint needs.</p>
+  </>
+
   );
 }
 
