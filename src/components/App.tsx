84d94ce9e5dd2dc57db1d5090938e14e507e6454
Mark Hollomon <mhhollomon@gmail.com>
2024-05-19 13:04:28 -0400

    Switch to Vite

---
+++ src/components/App.tsx
@@ -0,0 +1,24 @@
+//import { useState } from 'react'
+import logo from '../assets/cantus-icon.jpg'
+
+function App() {
+
+  return (
+    <>
+    <header className="container-fluid gx-0 bg-primary-subtle">
+      <div className="row align-items-center m-3">
+        <img src={logo} height="100px" width="100px"
+          className="header-logo" />
+        <div className="col-8 text-center">
+          <span className="text-black fs-1 p-3 p-md-4">Cantus Fortuitus</span>
+        </div>
+      </div>
+    </header>
+
+    <h3>Randomly Generated Cantus Firmus</h3>
+    <p>For all your counterpoint needs.</p>
+    </>
+  )
+}
+
+export default App
