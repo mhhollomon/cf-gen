48f1469ca2216c9f977e5868f4bdbcca7cdba967
Mark Hollomon <mhhollomon@gmail.com>
2026-01-19 10:02:54 -0500

    start rewrite in react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from "./components/header"
+//import CommonChords from "./components/common-chords"
+
+function App() {
+
+  return (
+    <>
+    <Header title="Cantus Fortuitus" avatar="notes-logo.svg"/>
+    {/* <CommonChords /> */}
+    </>
+  )
+}
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-})
+
+export default defineConfig(({ command, mode }) => {
+    const isProduction = (command === 'build' && mode === 'production');
+
+    return {
+        base: (isProduction ? '/common-chords/' : ''),
+        baseUrl: (isProduction ? '/common-chords/' : ''),
+        plugins: [react(), tsconfigPaths()],
+        build: {
+            outDir: 'build-web-deploy',
+            cssMinify: 'lightningcss',
+        },
+        css : {
+            transformer: 'lightningcss'
+        }
+
+    };
+});
